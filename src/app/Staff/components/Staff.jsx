9b333b61629ece192307f1c9c82f746d62afcade
PJ Lenhart <lenhartpj@gmail.com>
2022-11-25 10:40:53 -0500

    updating bios on staff page

--- src/app/Staff/components/Staff.jsx
+++ src/app/Staff/components/Staff.jsx
@@ -12,19 +12,19 @@ const Staff = () => {
       img: pj,
       title: "Coach PJ Lenhart - Head Coach",
       subtitle:
-        "Coach PJ has been head coach for 2 years after previously being an assistant coach for 3 years, all at Towson High. In High School, Coach PJ wrestled for Towson High for 2 years in the 182 and 195 pound weight class. Last year, the varsity team produced four state qualifiers and one state placer, our first in 9 years.",
+        "Coach PJ has been head coach since 2020 after previously being an assistant coach from 2017-2020, all at Towson High. In High School, Coach PJ wrestled for Towson High for 2 years in the 182 and 195 pound weight class. Last year, the varsity team produced four state qualifiers and one state placer, our first in 9 years. Coach PJ currently is a Software Engineer specializing in web development which allowed him the skills and opportunity to create this website for the program.",
     },
     {
       img: phil,
       title: "Coach Phil Simmonds - Head JV Coach",
       subtitle:
-        "Coach Simmonds has been head JV coach for 2 years after serving the previous 19 years as Varsity Head Coach of Towson High. Coach Simmonds has an incredible amount of wrestling knowledge having been around the sport for most of his life.  During his time as Towson, Coach Simmonds has produced 2 state champions and several other medalists at various levels. Coach Simmonds is an alumni of Baltimore Polytechnic High School.",
+        "Coach Simmonds has been head JV coach since 2020 after serving as Varsity Head Coach of Towson High from 2000-2020. Coach Simmonds has an incredible amount of wrestling knowledge having been around the sport for most of his life.  During his time at Towson, Coach Simmonds has produced 2 state champions and several other medalists at various levels. Coach Simmonds is an alumni of Baltimore Polytechnic High School and a Baltimore native. He is also a Marine Corps veteran.",
     },
     {
       img: pete,
       title: "Coach Pete Zaleski - Varsity Assistant Coach",
       subtitle:
-        "Coach Pete has been varsity assistant coach for 1 year. Coach Pete wrestled all 4 years of his high school career at Towson High under Coach Simmonds and brings wrestling knowledge that helped him wrestle at a high level, including several regional championship tournament matches. With this, Coach Pete helped our varsity team send four wrestlers to the state championships with one placing 5th.",
+        "Coach Pete has been varsity assistant coach for 1 year. Coach Pete wrestled all 4 years of his high school career at Towson High under Coach Simmonds and brings wrestling knowledge that helped him wrestle at a high level, including several regional championship tournament matches. With this, Coach Pete helped our varsity team send four wrestlers to the state championships with one placing 5th. After high school, Coach Pete attended Shippensburg University where he obtained his Bachelor's degree in Exercise Science. This has helped take the program's conditioning and training to the next level.",
     },
   ];
 
